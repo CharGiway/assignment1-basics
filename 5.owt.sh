6f22bc924eb19999cd33499c8d07810fb3b0a778
caizhiwei.lamp <caizhiwei.lamp@bytedance.com>
2026-02-08 18:43:10 +0800

    更新
    Change-Id: I4937be782d05a0132559f8c0c7eb04d3ba04bc53

--- 5.owt.sh
+++ 5.owt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-VOCAB_SIZE=32000 BATCH_SIZE=128 MAX_STEPS=30000 WARMUP_ITERS=2000 COSINE_CYCLE_ITERS=30000 MAX_LR=6e-4 MIN_LR=3e-4 WEIGHT_DECAY=0.05 bash run_training.sh train artifacts/owt_tokens/train.npy artifacts/owt_tokens/valid.npy mps
+VOCAB_SIZE=32000 BATCH_SIZE=128 MAX_STEPS=30000 WARMUP_ITERS=3000 COSINE_CYCLE_ITERS=30000 MAX_LR=6e-4 MIN_LR=3e-4 WEIGHT_DECAY=0.05 bash run_training.sh train artifacts/owt_tokens/train.npy artifacts/owt_tokens/valid.npy mps
