a8e9d4eda85327008e9136362d5b2db69ab1bad5
caizhiwei.lamp <caizhiwei.lamp@bytedance.com>
2026-02-08 21:08:37 +0800

    更新
    Change-Id: I89b7219c3fdcf336cb494c1974020c6e9db46808

--- run_train_tokenizer.sh
+++ run_train_tokenizer.sh
@@ -7,16 +7,16 @@ else
   NWORKERS=$(sysctl -n hw.ncpu 2>/dev/null || getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)
 fi
 
-uv run cs336_basics/train_bpe_tinystories.py \
-  --input_path data/TinyStoriesV2-GPT4-train.txt \
-  --vocab_size 10000 \
-  --out_dir artifacts \
-  --n_workers "$NWORKERS" \
-  --profile
-
 # uv run cs336_basics/train_bpe_tinystories.py \
-#   --input_path data/owt_train.txt \
-#   --vocab_size 32000 \
-#   --out_dir artifacts/owt_32k \
+#   --input_path data/TinyStoriesV2-GPT4-train.txt \
+#   --vocab_size 10000 \
+#   --out_dir artifacts \
 #   --n_workers "$NWORKERS" \
 #   --profile
+
+uv run cs336_basics/train_bpe_tinystories.py \
+  --input_path data/owt_train.txt \
+  --vocab_size 32000 \
+  --out_dir artifacts/owt_tokens \
+  --n_workers "$NWORKERS" \
+  --profile
