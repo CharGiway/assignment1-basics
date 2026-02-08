992a26618067ede9e7953c6131cd593c38159702
caizhiwei.lamp <caizhiwei.lamp@bytedance.com>
2026-02-08 11:06:13 +0800

    更新
    Change-Id: I44d6422c4c1cc7ac68fedfc86f32a7b484803ced

---
+++ no_rms.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+BATCH_SIZE=128 MAX_STEPS=5000 WARMUP_ITERS=2000 COSINE_CYCLE_ITERS=5000 MAX_LR=6e-4 MIN_LR=3e-4 WEIGHT_DECAY=0.05 VOCAB_SIZE=10000 NO_RMSNORM=1 bash run_training.sh train artifacts/tinystories_tokens/train.npy artifacts/tinystories_tokens/valid.npy mps
